4e0520fc0e2ee55cb1efb21f98752f8116f020dd
Bryan Velastegui <bryan_slvr@hotmail.com>
2015-07-31 11:43:26 -0500

    Creamos nuestro modelo Usuario pre-configurado para nuestra tabla de usuarios

---
+++ app/Usuario.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model implements AuthenticatableContract, CanResetPasswordContract {
+
+	/**
+	 * Tabla de la base de datos usada en el Modelo
+	 * @var string
+	 */
+	protected $table = 'usuario';
+
+	/**
+	 * Llave primaria para el modelo.
+	 * @var string
+	 */
+	protected $primaryKey = 'serial_usr';
+
+	/**
+	 * Los atributos que son accesibles.
+	 * @var array
+	 */
+	protected $fillable = ['serial_usr', 'login_usr', 'passw_usr', 'tipo_usr', 'rev_usr', 'ingreso_usr'];
+
+	/**
+	 * Los atributos excluidos del modelo JSON.
+	 * @var array
+	 */
+	protected $hidden = ['passw_usr', 'remember_token'];
+
+	/**
+	 * Obtener el identificador único para el usuario.
+	 * @return mixed
+	 */
+	public function getAuthIdentifier() {
+		return $this->getKey();
+	}
+
+	/**
+	 * Obtener la contraseña para el usuario.
+	 * @return string
+	 */
+	public function getAuthPassword() {
+		return $this->passw_usr;
+	}
+
+	/**
+	 * Obtener el valor simbólico "acuérdate de mí" para la sesión.
+	 * @return string
+	 */
+	public function getRememberToken() {
+		return $this->{$this->getRememberTokenName()};
+	}
+
+	/**
+	 * Definir el valor simbólico "acuérdate de mí" para la sesión.
+	 * @param  string $value
+	 * @return void
+	 */
+	public function setRememberToken($value) {
+		$this->{$this->getRememberTokenName()} = $value;
+	}
+
+	/**
+	 * Obtener el nombre de la columna para el token "remember me".
+	 * @return string
+	 */
+	public function getRememberTokenName() {
+		return 'remember_token';
+	}
+
+	/**
+	 * Obtener la dirección de correo electrónico a donde se envían enlaces de restablecimiento de contraseña.
+	 * @return string
+	 */
+	public function getEmailForPasswordReset() {
+		//por implementar
+	}
+}
